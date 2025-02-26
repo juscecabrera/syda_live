22d85ae5cdc5863a64579bcc20ffc22c7aa77f52
Jusce Cabrera <jusce240@gmail.com>
2025-02-26 02:58:38 -0500

    add env and change layouts

--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -1,18 +1,62 @@
-import StreamWithChat from "./StreamContainer";
+'use client'
+
+import { useState } from "react";
 
 const MainPage = () => {
+  const [layoutOptions, setLayoutOptions] = useState(1)
+
+
   const daarickStream = "https://player.kick.com/daarick"
   const daarickChat = "https://kick.com/popout/daarick/chat"
 
-  const sylveeStream = "https://player.twitch.tv/?channel=sylveey&parent=localhost"
-  const sylveeChat = "https://www.twitch.tv/embed/sylveey/chat?parent=localhost"
+  const sylveeStream = `https://player.twitch.tv/?channel=sylvee&parent=${process.env.PARENT_LINK}`
+  const sylveeChat = `https://www.twitch.tv/embed/sylvee/chat?parent=${process.env.PARENT_LINK}`
   
+  // El chat de sylvee, a la hora de hacer un raid, da error. 
+  // Se arregla si se actualiza. 
+  // No ganas los puntos porque no te fuiste al otro stream y el chat se queda vacio porque todos se fueron al otro stream raideado
+
+
+  //No permite iniciar sesion en Kick pero en Twitch si
   return (
-    <div>
-        <div className="w-screen h-screen flex flex-col justify-center items-center">
-          <StreamWithChat streamUrl={daarickStream} chatUrl={daarickChat}/>
-          <StreamWithChat streamUrl={sylveeStream} chatUrl={sylveeChat} />
+    <div className="bg-black">
+    {/* // Layout 1: Stream izquierda, Chat derecha (streams verticales) */}
+
+        <div className='w-screen h-screen flex flex-col justify-center items-center'>
+          {/* Daarick */}
+          <div className="w-[80%] lg:w-full h-[45%] flex flex-row justify-center">
+            <iframe 
+              src={daarickStream} 
+              allowFullScreen 
+              className="w-[39%] overflow-scroll h-[100%]"
+            ></iframe>
+            <iframe 
+              src={daarickChat} 
+              allowFullScreen 
+              className="w-1/5 h-full"
+            ></iframe>
+          </div>
+
+
+          {/* Sylvee */}
+          <div className="w-[80%] lg:w-full h-[45%] flex flex-row justify-center">
+            <iframe 
+              src={sylveeStream} 
+              allowFullScreen 
+              className="w-[39%] overflow-scroll h-[100%] object-fit"
+            ></iframe>
+            <iframe 
+              src={sylveeChat} 
+              allowFullScreen 
+              className="w-1/5 h-full"
+            ></iframe>
+          </div>
+          {/* <StreamWithChat streamUrl={daarickStream} chatUrl={daarickChat}/>
+          <StreamWithChat streamUrl={sylveeStream} chatUrl={sylveeChat} /> */}
         </div>
+
+  {/* Layout 2: Stream izquierda, Stream derecha, chats abajo (streams horizontales) */}
+
     </div>
   );
 };
--- components/StreamContainer.tsx
+++ components/StreamContainer.tsx
@@ -6,19 +6,24 @@ interface StreamWithChatProps {
 }
 
 const StreamWithChat: React.FC<StreamWithChatProps> = ({ streamUrl, chatUrl }) => {
+  
   return (
+    <>
     <div className="w-[80%] lg:w-full h-[45%] flex flex-row justify-center">
       <iframe 
         src={streamUrl} 
         allowFullScreen 
-        className="w-[48%] h-full"
+        className="w-[39%] overflow-scroll h-[100%] object-fit"
       ></iframe>
       <iframe 
         src={chatUrl} 
         allowFullScreen 
-        className="w-1/4 h-full"
+        className="w-1/5 h-full"
       ></iframe>
     </div>
+
+
+    </>
   );
 };
 
