035e2c9cf2cfa6f943524aa162e8a490d189a15e
Jusce <jusce240@gmail.com>
2025-07-27 01:20:54 -0500

    final commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Providers from "@/components/Providers";
+// import Providers from "@/components/Providers";
 
 
 const geistSans = Geist({
@@ -29,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>{children}</Providers>
+          {children}
+        {/* <Providers>
+          </Providers> */}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@ import WindowSizeWrapper from '@/components/WindowSizeWrapper';
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-black flex items-center justify-center w-screen h-screen p-3'>
       <WindowSizeWrapper />
-      <MainPage />
+      <h1 className='text-white font-bold text-3xl xs:text-2xl break-words'>
+        En memoria del Syda (2024-2025) 
+      </h1>
+      {/* <MainPage /> */}
     </div>
   );
 }
